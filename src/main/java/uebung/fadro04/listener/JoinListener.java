1aa6fc5164ac1c9427716cbd96a26a3902ef0c4d
Fadro04 <fabian732@schule.bremen.de>
2023-02-21 11:57:11 +0100

    datenbank

--- src/main/java/uebung/fadro04/listener/JoinListener.java
+++ src/main/java/uebung/fadro04/listener/JoinListener.java
@@ -3,16 +3,48 @@ package uebung.fadro04.listener;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.sql.*;
+
 public class JoinListener implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent event) {
+    public void onJoin(PlayerJoinEvent event) throws SQLException {
+        joinMessage(event);
+        registerPlayer(event);
+
+    }
+
+    public void registerPlayer(PlayerJoinEvent event) throws SQLException {
+        Player p = event.getPlayer();
+        String datei = "Claiming.db";
+        String url = "jdbc:sqlite:" + datei;
+        Connection conn = DriverManager.getConnection(url);
+
+        try {
+            String sql = "Select player_id from Player";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+
+            if (resultSet.getString(2) == null) {
+                sql = "Insert into Player player_name" +
+                        "Values (" + p.getName();
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        } finally {
+            conn.close();
+        }
+    }
+
+    public void joinMessage(PlayerJoinEvent event) {
         Player p = event.getPlayer();
         p.playSound(p, Sound.ENTITY_PLAYER_LEVELUP, 1.5f, 0.5f);
-        event.setJoinMessage(ChatColor.GREEN + "+[" +event.getPlayer().getName() + "]");
+        event.setJoinMessage(ChatColor.GREEN + "+[" + event.getPlayer().getName() + "]");
     }
 }
---
+++ src/main/java/uebung/fadro04/listener/test.java
@@ -0,0 +1,31 @@
+package uebung.fadro04.listener;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+import java.sql.*;
+
+public class test {
+    public static void main(String[] args) throws SQLException {
+
+
+        String name = "Fadro04";
+        String datei = "Claiming.db";
+        String url = "jdbc:sqlite:" + datei;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            String sql = "Select player_id from Player";
+            PreparedStatement statement = conn.prepareStatement(sql);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+
+            if (resultSet.getString(2) == null) {
+                sql = "Insert into Player player_name" +
+                        "Values (" + name + ")";
+            }
+        } catch (SQLException e) {
+            throw new SQLException(e);
+        }
+
+    }
+}
